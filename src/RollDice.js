a943fadee5195262551e63f9f396f9fb25b856db
Attah David 'Joche <39128641+JocheAttah@users.noreply.github.com>
2020-05-20 15:13:44 +0100

    styled with animation

--- src/RollDice.js
+++ src/RollDice.js
@@ -25,7 +25,7 @@ class RollDice extends Component{
      const newDie1 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)]
      const newDie2 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)]
 
-    this.setState({ isRolling: true});
+    this.setState({die1: "d6", die2: "d6", isRolling: true});
     // settimer
     setTimeout(()=>this.setState({die1: newDie1, die2: newDie2, isRolling: false}), 1000)
   }
@@ -35,8 +35,8 @@ class RollDice extends Component{
     return (
       <div className='RollDice'>
         <div className= 'RollDice__Dice'>        
-          <Die face={this.state.die1}/>
-          <Die face={this.state.die2}/>
+          <Die face={this.state.die1} animation = {this.state.isRolling}/>
+          <Die face={this.state.die2} animation = {this.state.isRolling}/>
         </div>
         <button className='RollDice__button' onClick= {this.roll} disabled={this.state.isRolling}> 
         { this.state.isRolling ? 'Rolling...' : 'Roll Dice !'}
